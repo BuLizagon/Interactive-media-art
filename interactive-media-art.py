482404d4f3d5636bb8e25b299406360e871b65cc
BuLizagon <dichieasy1212@naver.com>
2024-05-21 15:21:34 +0900

    Extract one person coordinates

--- interactive-media-art.py
+++ interactive-media-art.py
@@ -39,6 +39,11 @@ user_eye_y = 0
 user_neck_x = 0
 user_neck_y = 0
 
+#이전 위치값
+pre_x = 0
+pre_y = 0
+
+
 while True:
 
     #message 초기화
@@ -71,105 +76,115 @@ while True:
             y1 = i[1]
             x2 = i[2]
             y2 = i[3]
-            area = abs(int(x2)-int(x1))*abs(int(y2)-int(y1))
-            
-            if area > max_area:
-                max_area = area
+
+            if len(max_box)==0:
+                pre_x = x
+                pre_y = y
                 max_box.append(x1)
                 max_box.append(y1)
 
                 max_box.append(x2)
                 max_box.append(y2)
+            
+            else:
+                if pre_x-10<=x<=pre_x+10 and pre_y-10<=y<=pre_y+10:
+                    max_box=[]
+                    max_box.append(x1)
+                    max_box.append(y1)
 
-            if len(max_box)!=0:
-                x1 = int(max_box[0])
-                x2 = int(max_box[2])
+                    max_box.append(x2)
+                    max_box.append(y2)
 
-                y1 = int(max_box[1])
-                y2 = int(max_box[3])
 
-                x = int((((x2+x1)/2)-320)/320*100)
-                # y = int(y1)
-                y = int((((y2+y1)/2)-240)/240*(-100)+20)
+        if len(max_box)!=0:
+            x1 = int(max_box[0])
+            x2 = int(max_box[2])
 
-                while True:
-                    #고개 동작할 때
-                    if end-start>=10:
-                        #눈동자 동작
-                        if int(x/10)==int(user_eye_x/10)==int(user_neck_x/10) and int(y/10)==int(user_eye_y/10)==int(user_neck_y/10):
-                            user_eye_x = x
-                            user_eye_y = y
-                            user_neck_x = x
-                            user_neck_y = y
-                            message = f"{user_eye_x},{user_eye_y}.{user_neck_x},{user_neck_y}.1\n"
-                            ser.write(message.encode())
-                            break
-                            
-                        else:
-                            #눈동자 동작
-                            if int(x/10)==int(user_eye_x/10):
-                                user_eye_x = x
-                            elif x>user_eye_x:
-                                user_eye_x += 5
-                            elif x<user_eye_x:
-                                user_eye_x -= 5
-                            
-                            if int(y/10)==int(user_eye_y/10):
-                                user_eye_y = y
-                            elif y>user_eye_y:
-                                user_eye_y += 5
-                            elif y<user_eye_y:
-                                user_eye_y -= 5
+            y1 = int(max_box[1])
+            y2 = int(max_box[3])
 
-                            #고개 동작
-                            if int(x/10)==int(user_neck_x/10):
-                                user_neck_x = x
-                            elif x>user_neck_x:
-                                user_neck_x += 1
-                            elif x<user_neck_x:
-                                user_neck_x -= 1
-                            
-                            if int(y/10)==int(user_neck_y/10):
-                                user_neck_y = y
-                            elif y>user_neck_y:
-                                user_neck_y += 1
-                            elif y<user_neck_y:
-                                user_neck_y -= 1
+            x = int((((x2+x1)/2)-320)/320*100)
+            # y = int(y1)
+            y = int((((y2+y1)/2)-240)/240*(-100)+20)
 
-                            message = f"{user_eye_x},{user_eye_y}.{user_neck_x},{user_neck_y}.1\n"
-                            ser.write(message.encode())
-                    #고개 동작 안 할 때
+            while True:
+                #고개 동작할 때
+                if end-start>=10:
+                    #눈동자 동작
+                    if int(x/10)==int(user_eye_x/10)==int(user_neck_x/10) and int(y/10)==int(user_eye_y/10)==int(user_neck_y/10):
+                        user_eye_x = x
+                        user_eye_y = y
+                        user_neck_x = x
+                        user_neck_y = y
+                        message = f"{user_eye_x},{user_eye_y}.{user_neck_x},{user_neck_y}.1\n"
+                        ser.write(message.encode())
+                        break
+                        
                     else:
                         #눈동자 동작
-                        if int(x/10)==int(user_eye_x/10) and int(y/10)==int(user_eye_y/10):
+                        if int(x/10)==int(user_eye_x/10):
                             user_eye_x = x
-                            user_eye_y = y
-                            message = f"{user_eye_x},{user_eye_y}.{user_neck_x},{user_neck_y}.0\n"
-                            ser.write(message.encode())
-                            break
-                            
-                        else:
-                            #눈동자 동작
-                            if int(x/10)==int(user_eye_x/10):
-                                user_eye_x = x
-                            elif x>user_eye_x:
-                                user_eye_x += 1
-                            elif x<user_eye_x:
-                                user_eye_x -= 1
-                            
-                            if int(y/10)==int(user_eye_y/10):
-                                user_eye_y = y
-                            elif y>user_eye_y:
-                                user_eye_y += 1
-                            elif y<user_eye_y:
-                                user_eye_y -= 1
-
-                            message = f"{user_eye_x},{user_eye_y}.{user_neck_x},{user_neck_y}.0\n"
-                            ser.write(message.encode())
+                        elif x>user_eye_x:
+                            user_eye_x += 5
+                        elif x<user_eye_x:
+                            user_eye_x -= 5
                         
-                cv2.circle(frame, (int((x1+x2)/2), int((y1+y2)/2)-20), 3, (0, 0, 255), 2)
+                        if int(y/10)==int(user_eye_y/10):
+                            user_eye_y = y
+                        elif y>user_eye_y:
+                            user_eye_y += 5
+                        elif y<user_eye_y:
+                            user_eye_y -= 5
 
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                        #고개 동작
+                        if int(x/10)==int(user_neck_x/10):
+                            user_neck_x = x
+                        elif x>user_neck_x:
+                            user_neck_x += 1
+                        elif x<user_neck_x:
+                            user_neck_x -= 1
+                        
+                        if int(y/10)==int(user_neck_y/10):
+                            user_neck_y = y
+                        elif y>user_neck_y:
+                            user_neck_y += 1
+                        elif y<user_neck_y:
+                            user_neck_y -= 1
+
+                        message = f"{user_eye_x},{user_eye_y}.{user_neck_x},{user_neck_y}.1\n"
+                        ser.write(message.encode())
+                #고개 동작 안 할 때
+                else:
+                    #눈동자 동작
+                    if int(x/10)==int(user_eye_x/10) and int(y/10)==int(user_eye_y/10):
+                        user_eye_x = x
+                        user_eye_y = y
+                        message = f"{user_eye_x},{user_eye_y}.{user_neck_x},{user_neck_y}.0\n"
+                        ser.write(message.encode())
+                        break
+                        
+                    else:
+                        #눈동자 동작
+                        if int(x/10)==int(user_eye_x/10):
+                            user_eye_x = x
+                        elif x>user_eye_x:
+                            user_eye_x += 1
+                        elif x<user_eye_x:
+                            user_eye_x -= 1
+                        
+                        if int(y/10)==int(user_eye_y/10):
+                            user_eye_y = y
+                        elif y>user_eye_y:
+                            user_eye_y += 1
+                        elif y<user_eye_y:
+                            user_eye_y -= 1
+
+                        message = f"{user_eye_x},{user_eye_y}.{user_neck_x},{user_neck_y}.0\n"
+                        ser.write(message.encode())
+                    
+            cv2.circle(frame, (int((x1+x2)/2), int((y1+y2)/2)-20), 3, (0, 0, 255), 2)
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
     
     #사용자 인식 없음
     else:
