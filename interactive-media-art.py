8314a281c9da1782311de7126efc7ae69dac3d49
BuLizagon <dichieasy1212@naver.com>
2024-06-14 11:34:06 +0900

    Window Size Adjustment

--- interactive-media-art.py
+++ interactive-media-art.py
@@ -230,9 +230,10 @@ while True:
     
 
         start = time.time()
-
+    
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
     cv2.imshow('frame', frame)
-
+    
     if cv2.waitKey(1) == ord('q'):
         break
 ser.close()
